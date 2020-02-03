75ca32a6954c0a4d5093e4198547a40981826e2d
Hamish <Hangsii@users.noreply.github.com>
2020-02-03 21:23:42 +0000

    #1 Change setup script to handle use of github collaborators instead of deploy keys
    Also, a requirements.txt is redundant with conda handling the environment.
    
    Finally, use this opportunity to change the name of utils to alana_utils - good names are good.

--- ayesaac/services/external_interface_bot/environment_setup.sh
+++ ayesaac/services/external_interface_bot/environment_setup.sh
@@ -1,28 +1,23 @@
 # Setup anaconda environment
 echo "Creating virtual environment..."
 echo
-conda create -n Ayesaac python=3.7 -y
+conda env create -f environment.yml;
 eval "$(conda shell.bash hook)"
-conda activate Ayesaac
+conda activate Ayebot
 
 # Use the separately shared private key to get the private code
 echo
 echo "Cloning utils..."
-echo
-eval $(ssh-agent)
-ssh-add utils_github_key.asc
-ssh-keyscan -H github.com >> /etc/ssh/ssh_known_hosts
-git clone git@github.com:Hangsii/secret_alana_utils.git utils
+
+# I don't know if you use ssh or a credential manager to handle this! Trying both
+git clone https://github.com/Hangsii/secret_alana_utils.git alana_utils
+git clone git@github.com:Hangsii/secret_alana_utils.git alana_utils
 
 # Install dependency packages
 echo
 echo "Installing supporting packages for utils..."
 echo
-cd utils && pip install -e .
+cd alana_utils && pip install -e .
 
-echo
-echo "Installing packages for main level..."
-echo
-
-cd $OLDPWD && pip install -r requirements.txt
+cd $OLDPWD
 echo "All done!"
