1145c6abfc4eb74866c1ed24a7f2a9c4ebc945be
Hamish MacKinnon <Hamish.MacKinnon@eu.medical.canon>
2020-03-07 20:49:58 +0000

    #2 (ASR): Use IBM ASR in the service using a prerecorded file
    The previously explored library, SpeechRecognition, seems to not be up for the task as it doesn't handle the change APIs from cloud vendors or Python versions from the last couple of years. That lib is going out.
    
    IBM Watson API key not committed here - solution to be made soon.
    Next step, get this to use input from a microphone.

--- ayesaac/services/automatic_speech_recognition/main.py
+++ ayesaac/services/automatic_speech_recognition/main.py
@@ -1,7 +1,47 @@
-
 from pprint import pprint
+# import speech_recognition  # Looks like speech_recognition is dead in the water
+from pathlib import Path
+
+from ibm_watson import SpeechToTextV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 from ayesaac.services_lib.queues.queue_manager import QueueManager
+
+
+def callback_impl(data):
+    # obtain audio from file:
+    # todo use microphone
+    project_root = Path(__file__).parent.parent.parent  # ayesaac
+    audio_file = project_root / 'data' / 'test' / 'test-audio-query.wav'
+    # recognize speech using IBM
+    keyfilename = Path(__file__).parent / 'ibm_key'  # i.e. a file in the automatic_speech_recognition directory
+
+    try:
+        url = 'https://api.eu-gb.speech-to-text.watson.cloud.ibm.com/instances/0c812d73-03ef-4209-a78e-b73c4781f85a'
+        with open(keyfilename, 'r') as keyfile:
+            key = keyfile.read()
+
+        authenticator = IAMAuthenticator(key)
+        transcriber = SpeechToTextV1(authenticator=authenticator)
+        transcriber.set_service_url(url)
+
+        with open(audio_file, 'rb') as audio:  # open prerecorded file as binary
+            try:
+                transcribed_to_json = transcriber.recognize(audio=audio)
+            except Exception as e:
+                print("Watson error; {0}".format(e))
+
+        text = str(transcribed_to_json)  # todo parse returned JSON
+        print("Watson thinks you said " + text)
+        data['query'] = text
+    except Exception as e:
+        print("ASR error; {0}".format(e))
+
+    # fallback
+    if 'query' not in data:
+        data["query"] = "Is there a person in the kitchen ?"  # n.b. ibm wouldn't put a '?' here
+
+    return data
 
 
 class AutomaticSpeechRecognition(object):
@@ -13,9 +53,17 @@ class AutomaticSpeechRecognition(object):
         self.queue_manager = QueueManager([self.__class__.__name__, "NaturalLanguageUnderstanding"])
 
     def callback(self, body, **_):
+        """
+        The function run by the queue manager when a message for this service reaches the front.
+
+        :param body: a dictionary containing info required. This service expects to be the first run, so
+                     only requires a "path_done" list to be inside.
+        :param _:    Other callbacks may take more params. They should be ignored in this case.
+        :return: None
+        """
         pprint(body)
-        if 'query' not in body:
-            body["query"] = "Is there a person in the kitchen ?"
+        body = callback_impl(body)
+
         pprint(body["query"])
         body["path_done"].append(self.__class__.__name__)
         self.queue_manager.publish("NaturalLanguageUnderstanding", body)
@@ -30,4 +78,9 @@ def main():
 
 
 if __name__ == "__main__":
+
+    # inserted testing code
+    # dicto = {}
+    # print(callback_impl(dicto))  # TODO install pyaudio with poetry
+
     main()
