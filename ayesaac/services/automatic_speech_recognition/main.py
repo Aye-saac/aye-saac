eebea43ab993c6634b301f2b6245e1e6bfc98aa3
Hamish MacKinnon <Hamish.MacKinnon@eu.medical.canon>
2020-03-09 16:01:00 +0000

    #2 (ASR): Use a microphone with IBM speech recognition.
    Some thought may be needed as to how this can be structured for sane testing without calling the IBM API every time.
    
    Also remaining: this only records when given a prompt by send_one_request but should listen without prompt while the service is running (probably).

--- ayesaac/services/automatic_speech_recognition/main.py
+++ ayesaac/services/automatic_speech_recognition/main.py
@@ -3,20 +3,30 @@ from pprint import pprint
 from pathlib import Path
 
 from ibm_watson import SpeechToTextV1
+from ibm_watson.websocket import RecognizeCallback, AudioSource
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 from ayesaac.services_lib.queues.queue_manager import QueueManager
+import ayesaac.services.automatic_speech_recognition.speech_recognition.microphone as mic
+import ayesaac.services.automatic_speech_recognition.speech_recognition.recognizer as reco
 
 
 def callback_impl(data):
     # obtain audio from file:
     # todo use microphone
+    r = reco.Recognizer()
+    with mic.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+    print("Ok - processing...")
     project_root = Path(__file__).parent.parent.parent  # ayesaac
-    audio_file = project_root / 'data' / 'test' / 'test-audio-query.wav'
+    # audio_file = project_root / 'data' / 'test' / 'test-audio-query.wav'
+    audio_file = audio.get_wav_data()
     # recognize speech using IBM
     keyfilename = Path(__file__).parent / 'ibm_key'  # i.e. a file in the automatic_speech_recognition directory
 
     try:
+        # this url is unique to HM's account
         url = 'https://api.eu-gb.speech-to-text.watson.cloud.ibm.com/instances/0c812d73-03ef-4209-a78e-b73c4781f85a'
         with open(keyfilename, 'r') as keyfile:
             key = keyfile.read()
@@ -25,17 +35,19 @@ def callback_impl(data):
         transcriber = SpeechToTextV1(authenticator=authenticator)
         transcriber.set_service_url(url)
 
-        with open(audio_file, 'rb') as audio:  # open prerecorded file as binary
-            try:
-                transcribed_to_json = transcriber.recognize(audio=audio)
-            except Exception as e:
-                print("Watson error; {0}".format(e))
+        # with open(audio_file, 'rb') as audio:  # open prerecorded file as binary
+        try:
+            transcribed_to_json = transcriber.recognize(audio=audio_file)
+        except Exception as e:
+            print("Watson error; {0}".format(e))
+            transcribed_to_json = []
 
-        text = str(transcribed_to_json)  # todo parse returned JSON
+        text = str(transcribed_to_json.get_result())  # todo parse returned JSON
         print("Watson thinks you said " + text)
         data['query'] = text
     except Exception as e:
         print("ASR error; {0}".format(e))
+        raise e
 
     # fallback
     if 'query' not in data:
@@ -78,9 +90,4 @@ def main():
 
 
 if __name__ == "__main__":
-
-    # inserted testing code
-    # dicto = {}
-    # print(callback_impl(dicto))  # TODO install pyaudio with poetry
-
     main()
