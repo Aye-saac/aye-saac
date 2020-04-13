31760838bcb60a0190865c2c8f9a40d9b4246e07
Hamish MacKinnon <Hamish.MacKinnon@eu.medical.canon>
2020-04-13 09:47:05 +0100

    #3 Add missing services to startup script, and startup the right web server
    Maintaining 2 of these scripts is another good reason to unify them in Python.

--- ayesaac/start_all_services_in_env.sh
+++ ayesaac/start_all_services_in_env.sh
@@ -4,7 +4,7 @@
 rm -f ayesaac/services_log/*.txt
 
 # start all the python scripts in the background and redirect their outputs in the services_log directory
-conda run -n aye-saac python -u -m ayesaac.services.external_interface_bot.app_interface > ayesaac/services_log/0_webserver.txt &
+conda run -n aye-saac python -u -m ayesaac.services.external_interface_bot.server > ayesaac/services_log/0_webserver.txt &
 conda run -n aye-saac python -u -m ayesaac.services.automatic_speech_recognition.main > ayesaac/services_log/1_automatic_speech_recognition.txt &
 conda run -n aye-saac python -u -m ayesaac.services.natural_language_understanding.main > ayesaac/services_log/2_natural_language_understanding.txt &
 conda run -n aye-saac python -u -m ayesaac.services.manager.main > ayesaac/services_log/3_manager.txt &
@@ -12,6 +12,9 @@ conda run -n aye-saac python -u -m ayesaac.services.camera_manager.main > ayesaa
 conda run -n aye-saac python -u -m ayesaac.services.webcam.main > ayesaac/services_log/4_1_webcam.txt &
 conda run -n aye-saac python -u -m ayesaac.services.webcam_bis.main > ayesaac/services_log/4_2_webcam.txt &
 conda run -n aye-saac python -u -m ayesaac.services.object_detection.main > ayesaac/services_log/5_object_detection.txt &
+conda run -n aye-saac python -u -m ayesaac.services.optical_character_recognition.main > ayesaac/services_log/5_0_optical_character_recognition.txt &
+conda run -n aye-saac python -u -m ayesaac.services.color_detection.main > ayesaac/services_log/5_1_color_detection.txt &
+conda run -n aye-saac python -u -m ayesaac.services.position_detection.main > ayesaac/services_log/5_1_position_detection.txt &
 conda run -n aye-saac python -u -m ayesaac.services.interpreter.main > ayesaac/services_log/6_interpreter.txt &
 conda run -n aye-saac python -u -m ayesaac.services.natural_language_generator.main > ayesaac/services_log/7_natural_language_generator.txt &
 conda run -n aye-saac python -u -m ayesaac.services.text_to_speech.main > ayesaac/services_log/8_text_to_speech.txt &
