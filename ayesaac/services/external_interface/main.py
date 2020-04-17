f3444ded08c203477f0458e8ce7b0a8a01b76ea3
Amit Parekh <amit@dresser.app>
2020-04-17 18:30:01 +0100

    feat: add new service to handle end of rabbitmq pipeline
    Data from the pipeline goes to this service, which then saves the data to a txt file so it can be queried by the flask server. I wasnt sure how else to do it because I dont think python can have a persistent state or something like react, and the only other way I could think of storing the data was within a database which wouldve just overcomplicated this further.

---
+++ ayesaac/services/external_interface/main.py
@@ -0,0 +1,37 @@
+from ayesaac.services_lib.queues.queue_manager import QueueManager
+import json
+from pathlib import Path
+
+
+class ExternalInterface(object):
+    def __init__(self):
+        project_root = Path(__file__).parent.parent.parent.parent
+        self.output_dir = f"{project_root}/output"
+
+        self.queue_manager = QueueManager([self.__class__.__name__])
+
+    def dump_output(self, body):
+        # Get unique key
+        key = body["uid"]
+
+        file_name = f"{self.output_dir}/{key}.txt"
+
+        with open(file_name, "w") as output_file:
+            json.dump(body, output_file)
+
+    def callback(self, body, **_):
+        body["path_done"].append(self.__class__.__name__)
+
+        self.dump_output(body)
+
+    def run(self):
+        self.queue_manager.start_consuming(self.__class__.__name__, self.callback)
+
+
+def main():
+    external_interface = ExternalInterface()
+    external_interface.run()
+
+
+if __name__ == "__main__":
+    main()
