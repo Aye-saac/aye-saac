02c4399d31220ff055f72f9fc3845554b9de65bc
Elisa <elisara1000@hotmail.com>
2021-03-08 15:01:45 +0000

    updated intents, retrained data

--- ayesaac/nlu_prep/train_nlu.py
+++ ayesaac/nlu_prep/train_nlu.py
@@ -2,12 +2,18 @@ from rasa.shared.nlu.training_data.loading import load_data
 from rasa.nlu.model import Trainer
 from rasa.nlu import config
 
+# list of paths of training files
+#data_path=[]
+
+#data_path.append('data/nlu_textual_queries.md')
+#data_path.append('data/nlu.md')
+
 data_path = 'data/nlu_textual_queries.md'
 # data_path = 'data/nlu.md'
 
+#for i in range(len(data_path)):
 training_data = load_data(data_path)
 trainer = Trainer(config.load())
 trainer.train(training_data)
-
 directory = '../data/models/rasa/nlu'
 trainer.persist(directory)
