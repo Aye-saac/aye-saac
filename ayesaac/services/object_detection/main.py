32c93e6eeff88c2b3b958b27eb8e5b4e769e103e
Katie Baker <katie.m.baker87@gmail.com>
2021-03-21 22:36:43 +0000

    Updated bbox plotting to include model name

--- ayesaac/services/object_detection/main.py
+++ ayesaac/services/object_detection/main.py
@@ -131,10 +131,11 @@ class ObjectDetection(object):
             bboxes = [obj["bbox"] for obj in objects]
             class_names = [obj["name"] for obj in objects]
             scores = [obj["confidence"] for obj in objects]
+            models = [obj["model"] for obj in objects]
             
             # draw the bounding boxes
             # (outputs image to docker/volumes/aye-saac_output_data/_data/bbox_[timestamp].png)
-            draw_bounding_boxes(image, bboxes, class_names, scores, prefix="bbox")
+            draw_bounding_boxes(image, bboxes, class_names, scores, models, prefix="bbox")
             
             # need to filter the results to remove massively overlapping object detections
             # (this can arise when different models identify the same object for example)
@@ -143,10 +144,11 @@ class ObjectDetection(object):
             bboxes = [obj["bbox"] for obj in objects]
             class_names = [obj["name"] for obj in objects]
             scores = [obj["confidence"] for obj in objects]
+            models = [obj["model"] for obj in objects]
 
             # draw the bounding boxes
             # (outputs image to docker/volumes/aye-saac_output_data/_data/bbox_[timestamp].png)
-            draw_bounding_boxes(image, bboxes, class_names, scores, prefix="bbox_filtered")
+            draw_bounding_boxes(image, bboxes, class_names, scores, models, prefix="bbox_filtered")
             
             # append the objects to all_objects
             all_objects.extend(objects)
