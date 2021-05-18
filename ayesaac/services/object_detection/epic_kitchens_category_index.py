ba49e545cac6393183fde114229f2a301ffcbab6
Katie Baker <katie.m.baker87@gmail.com>
2021-05-18 12:53:35 +0100

    Download epic kitchens labels for baseline model

--- ayesaac/services/object_detection/epic_kitchens_category_index.py
+++ ayesaac/services/object_detection/epic_kitchens_category_index.py
@@ -1,17 +1,20 @@
-import csv
-
 from ayesaac.utils.config import Config
 
-# Get the EPIC-KITCHENS CSV file
+# Get the EPIC-KITCHENS label file
 config = Config()
-csv_file = config.directory.data.joinpath("epic_kitchens").joinpath("EPIC_noun_classes.csv")
+label_file = open(config.directory.data.joinpath("epic_kitchens").joinpath("EPICKitchens_FasterRCNN_label_map.pbtxt"), 'r')
+lines = label_file.readlines()
 
-# Read the EPIC-KITCHENS categories from the CSV file into a dictionary
+# Read the EPIC-KITCHENS categories from the label file into a dictionary
 epic_kitchens_category_index = {}
-with open(csv_file, mode='r') as infile:
-    reader = csv.reader(infile)
-    next(reader) # skip header
-    for rows in reader:
-        id = int(rows[0])
-        name = rows[1]
-        epic_kitchens_category_index[id] = {"id": id, "name": name}
+item_id = None
+for line in lines:
+    line = line.strip()
+    
+    if line.startswith("id:"):
+        item_id = line.split(" ")[1]
+    
+    if line.startswith("name:"):
+        item_name = line.split(" ")[1]
+        item_name = item_name.replace("'", "")
+        epic_kitchens_category_index[int(item_id)] = {"id": int(item_id), "name": item_name}
--- scripts/download-epic-kitchens-model.sh
+++ scripts/download-epic-kitchens-model.sh
@@ -10,4 +10,4 @@ sed -i "s|DEFAULT@SECLEVEL=2|DEFAULT@SECLEVEL=1|g" /etc/ssl/openssl.cnf
 mkdir -p data/epic_kitchens && \
 	cd data/epic_kitchens && \
 	curl -L -o saved_model.pb https://data.bris.ac.uk/datasets/gw5q146a9dqd2t82jhu8noe3m/EPICKitchens-FasterRCNN-checkpoint/saved_model/saved_model.pb && \
-	curl -L -o EPIC_noun_classes.csv https://raw.githubusercontent.com/epic-kitchens/epic-kitchens-55-annotations/master/EPIC_noun_classes.csv
+	curl -L -o EPICKitchens_FasterRCNN_label_map.pbtxt https://data.bris.ac.uk/datasets/gw5q146a9dqd2t82jhu8noe3m/EPICKitchens_FasterRCNN_label_map.pbtxt
