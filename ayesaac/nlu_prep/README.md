a0b13a4e06f70cdae849f81cb68bf3dc1b8933bf
root <root@DESKTOP-R69UM1Q.localdomain>
2021-08-08 18:35:41 +0200

    updated NLU training readme, added option to automate moving model

--- ayesaac/nlu_prep/README.md
+++ ayesaac/nlu_prep/README.md
@@ -12,6 +12,13 @@ Run the following commands after meeting main project dependencies
 pip3 install --upgrade rasa
 ```
 
+If you are having trouble with dependency conflicts, try running
+
+```
+pip install rasa==2.2.7
+pip install rasa[spacy]==2.2.4
+```
+
 ### Before running
 
 Fill the nlu.md file in the data directory which is the file that contains the training data (See https://rasa.com/docs/rasa/nlu/training-data-format/ for more information on how to format this file).
@@ -20,8 +27,21 @@ Don't forget to fill the "entries" directory with every entry file you would nee
 
 ### Usage
 
+```
 python3 train_nlu.py
+```
 
-## Using trained model in Aye-Saac
+### Linking spaCy model
+
+```
+python -m spacy download en_core_web_md
+python -m spacy link en_core_web_md en
+```
+
+To link the spaCy model on windows 10:
+https://stackoverflow.com/questions/53505068/couldn-t-link-model-to-en-core-web-md-on-a-windows-10
+
+
+### Using trained model in Aye-Saac
 
 Move your newly trained model in `../data/models/rasa/nlu/` to `../../data/rasa/nlu/`
--- ayesaac/nlu_prep/train_nlu.py
+++ ayesaac/nlu_prep/train_nlu.py
@@ -3,10 +3,39 @@ from rasa.nlu.model import Trainer
 from rasa.nlu import config
 
 data_path = 'data/nlu_combi.md'
-# data_path = 'data/nlu_textual_queries.md'
+data_path = 'data/nlu_textual_queries.md'
 
 training_data = load_data(data_path)
 trainer = Trainer(config.load())
 trainer.train(training_data)
 directory = '../data/models/rasa/nlu'
 trainer.persist(directory)
+
+from pathlib import Path
+import datetime as dt
+from distutils.dir_util import copy_tree
+import os
+
+src = '../data/models/rasa/nlu'
+dst = '../../data/rasa/nlu'
+i = input("Copy latest model from {src} into {dst} ? (Y/n)\n".format(src=src,dst=dst))
+
+latest_modif_time = dt.datetime(1970,1,1)
+latest_modif_path = None
+
+if i == 'Y':
+    for path in Path(src).iterdir():
+        mtime = dt.datetime.fromtimestamp(path.stat().st_mtime)
+        if not ".DS_Store" in str(path) and mtime > latest_modif_time:
+            latest_modif_time = mtime
+            latest_modif_path = path
+
+    if not latest_modif_path == None:
+        (head, tail) = os.path.split(latest_modif_path)
+
+        src += "/" + tail
+        dst += "/" + tail
+
+        print("Copying model {src} into {dst}...".format(src=src, dst=dst))
+        copy_tree(src,dst)
+        print("Successfully copied model")
